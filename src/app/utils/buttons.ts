02b45e3bd9e18f5c611984f65a0536457026845d
Vitalii Skyrtach <vitalijskirtac@gmail.com>
2024-08-29 11:21:27 +0300

    -

--- src/app/utils/buttons.ts
+++ src/app/utils/buttons.ts
@@ -1,3 +1,18 @@
-export const addToCart = () => {
-  alert("addToCart");
+export const addToCart = (product: any) => {
+  // Дивимось на поточний вміст кошика з localStorage або створюємо порожній об'єкт в який будемо класти значення
+  let cart = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart") as string) : {};
+  // Перевіряємо чи продукт вже існує в кошику
+  if (cart[product.id]) {
+    // Якщо існує, збільшуємо кількість на 1
+    cart[product.id].quantity += 1;
+  } else {
+    // Якщо не існує, додаємо новий продукт з кількістю quantity 1
+    cart[product.id] = {
+      quantity: 1,
+    };
+  }
+  // Зберігаємо оновлений кошик у localStorage
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  console.log(`Product ID: ${product.id}, Quantity: ${cart[product.id].quantity}`);
 };
